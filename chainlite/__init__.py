93ffc67fb5c83b678bdc754cb5bc8842ea27d4b4
Sina <s.j.semnani@gmail.com>
2024-05-23 07:03:35 +0000

    Fix the format issue of the configuration file

--- chainlite/__init__.py
+++ chainlite/__init__.py
@@ -2,6 +2,7 @@ from .llm_generate import llm_generation_chain, pprint_chain, write_prompt_logs_
 from .utils import get_logger
 from .llm_config import load_config_from_file
 from .llm_config import get_total_cost
+from langchain_core.runnables import chain
 
 
-__all__ = ["llm_generation_chain", "get_logger", "load_config_from_file", "pprint_chain", "write_prompt_logs_to_file", "get_total_cost"]
+__all__ = ["llm_generation_chain", "get_logger", "load_config_from_file", "pprint_chain", "write_prompt_logs_to_file", "get_total_cost", "chain"]
